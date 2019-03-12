ff82b010fc2bc397f1697c200a2b3970311e9756
Filip Fonal <filipfonal@gmail.com>
2019-03-12 23:14:03 +0100

    Switching between fragments

--- app/src/main/java/com/tenants/tenants/MainActivity.kt
+++ app/src/main/java/com/tenants/tenants/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.tenants.tenants
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.tenants.tenants.Fragments.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(),
+    NavigationView.OnNavigationItemSelectedListener,
+    HomeFragment.OnFragmentInteractionListener,
+    BillsFragment.OnFragmentInteractionListener,
+    DebtsFragment.OnFragmentInteractionListener,
+    ShoppingListFragment.OnFragmentInteractionListener,
+    CleaningFragment.OnFragmentInteractionListener
+
+{
+    lateinit var billsFragment: BillsFragment
+    lateinit var cleaningFragment: CleaningFragment
+    lateinit var debtsFragment: DebtsFragment
+    lateinit var homeFragment: HomeFragment
+    lateinit var shoppingListFragment: ShoppingListFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +47,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+        homeFragment = HomeFragment.newInstance("", "")
+        debtsFragment = DebtsFragment.newInstance("", "")
+        shoppingListFragment = ShoppingListFragment.newInstance("", "")
+        billsFragment = BillsFragment.newInstance("", "")
+        cleaningFragment = CleaningFragment.newInstance("", "")
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, homeFragment)
+            .commit()
+
+        nav_view.setCheckedItem(R.id.sidebar_home)
     }
 
     override fun onBackPressed() {
@@ -60,23 +90,34 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // Handle navigation view item clicks here.
         when (item.itemId) {
             R.id.sidebar_home -> {
-                // Handle the camera action
+                switchFragment(homeFragment)
             }
             R.id.sidebar_debts -> {
-
+                switchFragment(debtsFragment)
             }
             R.id.sidebar_shopping_list -> {
-
+                switchFragment(shoppingListFragment)
             }
             R.id.sidebar_bills -> {
-
+                switchFragment(billsFragment)
             }
             R.id.sidebar_cleaning -> {
-
+                switchFragment(cleaningFragment)
             }
         }
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {}
+
+    fun switchFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, fragment)
+            .addToBackStack(fragment.toString())
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
     }
 }
