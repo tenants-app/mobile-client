5222e2b67b0737dc3dadf75eca3350286559da75
Filip Fonal <filipfonal@gmail.com>
2019-06-11 04:06:44 +0200

    Improve displaying products in shopping list

--- app/src/main/java/com/tenants/tenants/fragments/ShoppingListDetailsFragment.kt
+++ app/src/main/java/com/tenants/tenants/fragments/ShoppingListDetailsFragment.kt
@@ -59,7 +59,7 @@ class ShoppingListDetailsFragment : Fragment() {
 
     fun getProductTextView(productName: String, productValue: Int): TextView {
         val productTextView = TextView(baseContext)
-        productTextView.text = "● " + productName + " - " + productValue.toString() + "zł"
+        productTextView.text = "◦" + productName + " - " + productValue.toString() + "zł"
         productTextView.setTextSize(18F)
 
         return productTextView
